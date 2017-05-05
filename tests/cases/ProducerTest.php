a6f49c2ed5764e02fee6738ad80b7ddb7e50c301
Pavel Janda <me@paveljanda.com>
2017-05-06 01:13:50 +0200

    Dummy test code removed

--- tests/cases/ProducerTest.php
+++ tests/cases/ProducerTest.php
@@ -2,9 +2,6 @@
 
 namespace Gamee\RabbitMQ\Tests\Cases;
 
-use Bunny\Channel;
-use Bunny\Client;
-use Bunny\Message;
 use Tester\Assert;
 use Tester\TestCase;
 
@@ -13,54 +10,20 @@ require __DIR__ . '/../bootstrap.php';
 final class ProducerTest extends TestCase
 {
 
-	/**
-	 * @var Client
-	 */
-	private $bunny;
-
-
 	public function setUp()
 	{
-		$this->bunny = new Client([
-			'host' => 'localhost',
-			'port' => '5672'
-		]);
-		$this->bunny->connect();
+		
 	}
 
 
-	public function testPublish()
+	public function testFoo()
 	{
-		$channel = $this->bunny->channel();
-
-		$channel->queueDeclare('hello_durable', false, true, false, false);
-
-		$channel->publish('Hello', [], '', 'hello_durable');
-
-		echo "Sent 'Hello World!'\n";
-	}
-
-
-	public function testConsume()
-	{
-		$channel = $this->bunny->channel();
-
-		$channel->consume(
-			function (Message $message, Channel $channel, Client $client) {
-				var_dump($message->content);
-				$channel->ack($message);
-			},
-			'hello_durable'
-		);
-
-		$this->bunny->run(1);
+		Assert::true(true);
 	}
 
 
 	public function tearDown()
 	{
-		Assert::true(true);
-
 		$this->bunny->disconnect();
 	}
 
