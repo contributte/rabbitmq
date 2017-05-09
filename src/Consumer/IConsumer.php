fdc220b5d5aed9c59e617f1856803e3b57c59bfd
Pavel Janda <me@paveljanda.com>
2017-05-09 11:52:33 +0200

    Added simple interface IConsumer

---
+++ src/Consumer/IConsumer.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @copyright   Copyright (c) 2017 gameeapp.com <hello@gameeapp.com>
+ * @author      Pavel Janda <pavel@gameeapp.com>
+ * @package     Gamee
+ */
+
+namespace Gamee\RabbitMQ\Consumer;
+
+use Bunny\Message;
+
+interface IConsumer
+{
+
+	public function consume(Message $message): int;
+
+}
