9c964bbc5d7957ecb3fab6dc2e27439c9731c727
Karel Borkovec <info@karelborkovec.cz>
2019-04-15 15:29:38 +0200

    Added: possibility to reject message and stop consuming

--- src/Consumer/IConsumer.php
+++ src/Consumer/IConsumer.php
@@ -18,6 +18,7 @@ interface IConsumer
 	const MESSAGE_ACK = 1;
 	const MESSAGE_NACK = 2;
 	const MESSAGE_REJECT = 3;
+	const MESSAGE_REJECT_AND_TERMINATE = 4;
 
 	public function consume(Message $message): int;
 
